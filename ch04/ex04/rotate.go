38f2e101f91a8f75e4a87866fd7075bb5c8c632a
Shumpei IINUMA <shumpei.iinuma@gmail.com>
2018-08-16 03:32:16 +0900

    refactor: ch04, ex04

--- ch04/ex04/rotate.go
+++ ch04/ex04/rotate.go
@@ -5,7 +5,7 @@ func rotate(s []int, step int) {
 	step = step % lenS
 	i := 0
 	tmp := s[0]
-	for {
+	for range s {
 		// 移動先
 		dest := i - step
 		if dest < 0 {
@@ -13,10 +13,9 @@ func rotate(s []int, step int) {
 		} else if lenS <= dest {
 			dest = dest - lenS
 		}
+
+		// 移動
 		s[dest], tmp = tmp, s[dest]
 		i = dest
-		if i == 0 {
-			break
-		}
 	}
 }
