36fb2d7a715ed3303fa6ef71d29253f7c4afcf14
Shumpei IINUMA <shumpei.iinuma@gmail.com>
2018-08-18 17:18:22 +0900

    fix: ch04, ex04; ratate

--- ch04/ex04/rotate.go
+++ ch04/ex04/rotate.go
@@ -1,31 +1,27 @@
 package main
 
 func rotate(s []int, step int) {
-	lenS := len(s)
-	step = step % lenS
-
-	// 長さが偶数で、長さ/2の要素をずらす場合
-	if lenS == 2*step || lenS == -2*step {
-		for i := 0; i < lenS/2; i++ {
-			s[i], s[lenS/2+i] = s[lenS/2+i], s[i]
-		}
+	L := len(s)
+	// 0 < step < L
+	step = step % L
+	if step == 0 {
 		return
 	}
+	if step < 0 {
+		step = L + step
+	}
 
-	i := 0
-	tmp := s[0]
+	start := 0
+	dest, backup := start, s[start]
 	for range s {
-		// 移動先
-		dest := i - step
-		if dest < 0 {
-			dest = lenS + dest
-		} else if lenS <= dest {
-			dest = dest - lenS
+		src := (dest + step) % L
+		if src == start {
+			s[dest] = backup
+			start++
+			dest, backup = start, s[start]
+			continue
 		}
-
-		// iの要素をdestに移動、destの要素をtmpに退避
-		s[dest], tmp = tmp, s[dest]
-		// 次のループでdestの要素を移動する
-		i = dest
+		s[dest] = s[src]
+		dest = src
 	}
 }
