f6e2c042b9587f0c451253945fdfdffcae8a5f80
Shumpei IINUMA <shumpei.iinuma@gmail.com>
2018-09-10 01:23:26 +0900

    ch07; ex12

---
+++ ch07/ex12/main.go
@@ -0,0 +1,133 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"strconv"
+	"sync"
+)
+
+func main() {
+	db := database{items: map[string]dollars{"shoes": 50, "socks": 5}}
+	http.HandleFunc("/list", db.List)
+	http.HandleFunc("/price", db.Price)
+	http.HandleFunc("/create", db.Create)
+	http.HandleFunc("/read", db.Price)
+	http.HandleFunc("/update", db.Update)
+	http.HandleFunc("/delete", db.Delete)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+func (db *database) Create(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+	if len(item) == 0 {
+		http.Error(w, "item name is empty\n", http.StatusBadRequest)
+		return
+	}
+	price, err := strconv.ParseFloat(req.URL.Query().Get(`price`), 32)
+	if err != nil || price < 0 {
+		http.Error(
+			w,
+			fmt.Sprintf("invalid price: %q\n", req.URL.Query().Get(`price`)),
+			http.StatusBadRequest,
+		)
+		return
+	}
+
+	db.Lock()
+	defer db.Unlock()
+	if _, ok := db.items[item]; ok {
+		http.Error(w, fmt.Sprintf("item already exits: %q\n", item), http.StatusBadRequest)
+		return
+	}
+	db.items[item] = dollars(price)
+	fmt.Fprintf(w, "%s = %s", item, dollars(price))
+}
+
+func (db *database) Update(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+	if len(item) == 0 {
+		http.Error(w, "item name is empty\n", http.StatusBadRequest)
+		return
+	}
+	price, err := strconv.ParseFloat(req.URL.Query().Get(`price`), 32)
+	if err != nil || price < 0 {
+		http.Error(
+			w,
+			fmt.Sprintf("invalid price: %q\n", req.URL.Query().Get(`price`)),
+			http.StatusBadRequest,
+		)
+		return
+	}
+
+	db.Lock()
+	defer db.Unlock()
+	if _, ok := db.items[item]; !ok {
+		http.Error(w, fmt.Sprintf("no such item: %q\n", item), http.StatusBadRequest)
+		return
+	}
+	db.items[item] = dollars(price)
+	fmt.Fprintf(w, "%s = %s", item, dollars(price))
+}
+
+func (db *database) Delete(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+	if len(item) == 0 {
+		http.Error(w, "item name is empty\n", http.StatusBadRequest)
+		return
+	}
+
+	db.Lock()
+	defer db.Unlock()
+	if _, ok := db.items[item]; !ok {
+		http.Error(w, fmt.Sprintf("no such item: %q\n", item), http.StatusBadRequest)
+		return
+	}
+	delete(db.items, item)
+	fmt.Fprintf(w, "%s is deleted\n", item)
+}
+
+var htmlTemplate = template.Must(template.New("htmlTemplate").Parse(`
+<table>
+<tr style='text-align: left'>
+  <th>item</th>
+  <th>price</th>
+</tr>
+{{range $name, $price := .}}
+<tr>
+  <td>{{$name}}</td>
+  <td>{{$price}}</td>
+</tr>
+{{end}}
+`))
+
+func (db *database) List(w http.ResponseWriter, req *http.Request) {
+	db.RLock()
+	defer db.RUnlock()
+	if err := htmlTemplate.Execute(w, db.items); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func (db *database) Price(w http.ResponseWriter, req *http.Request) {
+	db.RLock()
+	defer db.RUnlock()
+	item := req.URL.Query().Get("item")
+	if price, ok := db.items[item]; ok {
+		fmt.Fprintf(w, "%s\n", price)
+	} else {
+		http.Error(w, fmt.Sprintf("no such item: %q\n", item), http.StatusNotFound)
+
+	}
+}
+
+type dollars float32
+
+func (d dollars) String() string { return fmt.Sprintf("$%.2f", d) }
+
+type database struct {
+	sync.RWMutex
+	items map[string]dollars
+}
