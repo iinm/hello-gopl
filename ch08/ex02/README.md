02cde5acf76e2a58313b46b13e5556ae5adba285
Shumpei IINUMA <shumpei.iinuma@gmail.com>
2018-10-26 02:39:36 +0900

    ch08, ex02 参考資料のリンク

---
+++ ch08/ex02/README.md
@@ -0,0 +1,4 @@
+参考：
+- https://tools.ietf.org/html/rfc959
+- https://www.ncftp.com/libncftp/doc/ftp_overview.html
+- https://en.wikipedia.org/wiki/List_of_FTP_server_return_codes
--- ch08/ex02/server.go
+++ ch08/ex02/server.go
@@ -4,9 +4,12 @@ import (
 	"bufio"
 	"bytes"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net"
+	"os"
+	"path/filepath"
 	"strconv"
 	"strings"
 )
@@ -22,7 +25,35 @@ func main() {
 			log.Print(err)
 			continue
 		}
-		handleConn(conn)
+		go handleConn(conn)
+	}
+}
+
+func handleConn(conn net.Conn) {
+	c, err := NewClient(conn)
+	if err != nil {
+		log.Print(err)
+		fmt.Fprint(conn, "421 Service not available, closing control connection\r\n")
+		conn.Close()
+		return
+	}
+
+	c.reply("220 Service Ready")
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		line := scanner.Text()
+		log.Printf("%s: %q", conn.RemoteAddr(), line)
+		ss := strings.Split(line, " ")
+		if len(ss) == 0 {
+			c.reply("501 Syntax error in parameters or arguments")
+			continue
+		}
+		cmd := ss[0]
+		var args []string
+		if len(ss) >= 2 {
+			args = ss[1:]
+		}
+		c.request(cmd, args)
 	}
 }
 
@@ -32,6 +63,54 @@ type client struct {
 	dataAddress string
 	username    string
 	authorized  bool
+	workdir     string
+	initWorkdir string // このディレクトリ配下のみアクセス可能にする
+	dataType    string
+}
+
+func NewClient(conn net.Conn) (*client, error) {
+	workdir, err := filepath.Abs("./")
+	if err != nil {
+		return nil, err
+	}
+	c := &client{
+		conn:        conn,
+		dataType:    "A",
+		workdir:     workdir,
+		initWorkdir: workdir,
+	}
+	return c, nil
+}
+
+func (c *client) reply(s string) {
+	_, err := c.conn.Write([]byte(s + "\r\n"))
+	if err != nil {
+		log.Print(err)
+	}
+}
+
+func (c *client) replyf(s string, a ...interface{}) {
+	c.reply(fmt.Sprintf(s, a...))
+}
+
+func (c *client) transfer(r io.Reader) {
+	c.replyf("150 Opening (%s) type data connection", c.dataType)
+	var err error
+	c.dataConn, err = net.Dial("tcp", c.dataAddress)
+	if err != nil {
+		log.Print(err)
+		c.reply("425 Can't open data connection")
+		return
+	}
+	defer c.dataConn.Close()
+	if c.dataType == "A" {
+		r = convertToAscii(r)
+	}
+	_, err = io.Copy(c.dataConn, r)
+	if err != nil {
+		log.Print(err)
+	}
+	c.reply("226 Closing data connection")
 }
 
 func (c *client) request(cmd string, args []string) {
@@ -40,130 +119,163 @@ func (c *client) request(cmd string, args []string) {
 	switch strings.ToLower(cmd) {
 	case "user":
 		if len(args) != 1 {
-			reply(c.conn, "501 Syntax error in parameters or arguments")
+			c.reply("501 Syntax error in parameters or arguments")
 			return
 		}
 		c.username = args[0]
-		reply(c.conn, "331 Anonymous login ok, send complete email address as your password")
+		c.reply("331 Anonymous login ok, send complete email address as your password")
 
 	case "pass":
 		c.authorized = true
-		reply(c.conn, "230 Anonymous User logged in")
+		c.reply("230 Anonymous User logged in")
 
 	case "syst":
-		reply(c.conn, "215 UNIX")
+		c.reply("215 UNIX")
 
 	case "port":
 		if len(args) != 1 {
-			reply(c.conn, "501 Syntax error in parameters or arguments")
+			c.reply("501 Syntax error in parameters or arguments")
+			return
+		}
+		ss := strings.Split(args[0], ",")
+		if len(ss) != 6 {
+			c.reply("501 Syntax error in parameters or arguments")
 			return
 		}
 		numbers := make([]int, 0)
-		for _, s := range strings.Split(args[0], ",") {
+		for _, s := range ss {
 			n, err := strconv.Atoi(s)
 			if err != nil {
 				log.Print(err)
-				reply(c.conn, "501 Syntax error in parameters or arguments")
+				c.reply("501 Syntax error in parameters or arguments")
 				return
 			}
 			numbers = append(numbers, n)
 		}
 		c.dataAddress = fmt.Sprintf("%d.%d.%d.%d:%d",
 			numbers[0], numbers[1], numbers[2], numbers[3], numbers[4]*256+numbers[5])
-		reply(c.conn, "200 Command OK")
+		c.reply("200 Command OK")
 
-	case "list":
-		reply(c.conn, "150 Opening ASCII mode data connection")
-		var err error
-		c.dataConn, err = net.Dial("tcp", c.dataAddress)
+	case "pwd":
+		c.replyf("257 %q", c.workdir)
+
+	case "cwd":
+		if len(args) != 1 {
+			c.reply("501 Syntax error in parameters or arguments")
+			return
+		}
+		dir := args[0]
+		if !strings.HasPrefix(dir, "/") {
+			dir = filepath.Join(c.workdir, dir)
+		}
+		abspath, err := filepath.Abs(dir)
 		if err != nil {
 			log.Print(err)
-			reply(c.conn, "425 Can't open data connection")
+			c.replyf("550 %s", err)
+			return
+		}
+		if !strings.HasPrefix(abspath, c.initWorkdir) {
+			c.reply("550 Requested action not taken. File unavailable")
+			return
+		}
+		if _, err := os.Stat(abspath); os.IsNotExist(err) {
+			log.Print(err)
+			c.replyf("550 %s", err)
 			return
 		}
 
-		buf := new(bytes.Buffer)
-		files, err := ioutil.ReadDir("./")
+		c.workdir = abspath
+		c.replyf("200 directory changed to %s", c.workdir)
+
+	case "list":
+		dir := c.workdir
+		if len(args) == 1 {
+			dir = args[0]
+		}
+		if !strings.HasPrefix(dir, "/") {
+			dir = filepath.Join(c.workdir, dir)
+		}
+		abspath, err := filepath.Abs(dir)
 		if err != nil {
-			log.Fatal(err)
+			log.Print(err)
+			c.replyf("550 %s", err)
+			return
+		}
+		if !strings.HasPrefix(abspath, c.initWorkdir) {
+			c.reply("550 Requested action not taken. File unavailable")
+			return
+		}
+		if _, err := os.Stat(abspath); os.IsNotExist(err) {
+			log.Print(err)
+			c.replyf("550 %s", err)
+			return
+		}
+		buf := new(bytes.Buffer)
+		files, err := ioutil.ReadDir(abspath)
+		if err != nil {
+			log.Print(err)
+			c.replyf("550 %s", err)
+			return
 		}
 		for _, f := range files {
 			buf.WriteString(f.Name() + "\r\n")
 		}
-
-		_, err = c.dataConn.Write(buf.Bytes())
-		if err != nil {
-			log.Print(err)
-		}
-		c.dataConn.Close()
-		reply(c.conn, "226 Closing data connection")
+		c.transfer(buf)
 
 	case "retr":
 		if len(args) != 1 {
-			reply(c.conn, "501 Syntax error in parameters or arguments")
+			c.reply("501 Syntax error in parameters or arguments")
 			return
 		}
-		reply(c.conn, "150 Opening ASCII mode data connection")
-		var err error
-		c.dataConn, err = net.Dial("tcp", c.dataAddress)
+		filename := filepath.Join(c.workdir, args[0])
+		abspath, err := filepath.Abs(filename)
 		if err != nil {
 			log.Print(err)
-			reply(c.conn, "425 Can't open data connection")
+			c.replyf("550 %s", err)
+			return
+		}
+		if !strings.HasPrefix(abspath, c.initWorkdir) {
+			c.reply("550 Requested action not taken. File unavailable")
+			return
+		}
+		if _, err := os.Stat(abspath); os.IsNotExist(err) {
+			log.Print(err)
+			c.replyf("550 %s", err)
 			return
 		}
 
-		content, err := ioutil.ReadFile("./" + args[0])
-		if err != nil {
-			log.Fatal(err)
-		}
-		_, err = c.dataConn.Write(content)
+		file, err := os.Open(abspath)
 		if err != nil {
 			log.Print(err)
+			c.replyf("550 %s", err)
+			return
 		}
-		c.dataConn.Close()
-		reply(c.conn, "226 Closing data connection")
+		defer file.Close()
+		c.transfer(file)
+
+	case "type":
+		if len(args) != 1 || (args[0] != "A" && args[0] != "I") {
+			c.reply("501 Syntax error in parameters or arguments")
+			return
+		}
+		c.dataType = args[0]
+		c.replyf("200 data type changed to %s", c.dataType)
 
 	case "quit":
-		reply(c.conn, "221 Goodbye")
+		c.reply("221 Goodbye")
 		c.conn.Close()
 
 	default:
-		reply(c.conn, "502 Command not implemented")
+		c.reply("502 Command not implemented")
 	}
 }
 
-func handleConn(conn net.Conn) {
-	in := make(chan string)
-	cl := &client{conn: conn}
-	go recieve(conn, in)
-	reply(conn, "220 Service Ready")
-	for s := range in {
-		ss := strings.Split(s, " ")
-		if len(ss) == 0 {
-			continue
-		}
-		cmd := ss[0]
-		var args []string
-		if len(ss) >= 2 {
-			args = ss[1:]
-		}
-		cl.request(cmd, args)
+func convertToAscii(r io.Reader) io.Reader {
+	s := bufio.NewScanner(r)
+	buf := new(bytes.Buffer)
+	for s.Scan() {
+		buf.WriteString(s.Text())
+		buf.WriteString("\r\n")
 	}
-}
-
-func recieve(conn net.Conn, out chan<- string) {
-	scanner := bufio.NewScanner(conn)
-	for scanner.Scan() {
-		line := scanner.Text()
-		log.Printf("%s: %q", conn.RemoteAddr(), line)
-		out <- line
-	}
-	//conn.Close()
-}
-
-func reply(conn net.Conn, s string) {
-	_, err := conn.Write([]byte(s + "\r\n"))
-	if err != nil {
-		log.Print(err)
-	}
+	return buf
 }
