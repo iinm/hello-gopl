283fe95b146bf888c3670f2430db32a0531fa073
Shumpei IINUMA <shumpei.iinuma@gmail.com>
2018-12-21 06:10:13 +0900

    ch12, ex13; decoder

---
+++ ch12/ex13/sexpr/decode.go
@@ -0,0 +1,148 @@
+package sexpr
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"reflect"
+	"strconv"
+	"strings"
+	"text/scanner"
+)
+
+type Decoder struct {
+	r io.Reader
+}
+
+func NewDecoder(r io.Reader) *Decoder {
+	return &Decoder{r}
+}
+
+func (dec *Decoder) Decode(v interface{}) (err error) {
+	lex := &lexer{scan: scanner.Scanner{Mode: scanner.GoTokens}}
+	lex.scan.Init(dec.r)
+	lex.next()
+	defer func() {
+		if x := recover(); x != nil {
+			err = fmt.Errorf("error at %s: %v", lex.scan.Position, x)
+		}
+	}()
+	read(lex, reflect.ValueOf(v).Elem())
+	return nil
+}
+
+func Unmarshal(data []byte, out interface{}) (err error) {
+	lex := &lexer{scan: scanner.Scanner{Mode: scanner.GoTokens}}
+	lex.scan.Init(bytes.NewReader(data))
+	lex.next()
+	defer func() {
+		if x := recover(); x != nil {
+			err = fmt.Errorf("error at %s: %v", lex.scan.Position, x)
+		}
+	}()
+	read(lex, reflect.ValueOf(out).Elem())
+	return nil
+}
+
+type lexer struct {
+	scan  scanner.Scanner
+	token rune
+}
+
+func (lex *lexer) next()        { lex.token = lex.scan.Scan() }
+func (lex *lexer) text() string { return lex.scan.TokenText() }
+
+func (lex *lexer) consume(want rune) {
+	if lex.token != want {
+		panic(fmt.Sprintf("got %q, want %q", lex.text(), want))
+	}
+	lex.next()
+}
+
+func read(lex *lexer, v reflect.Value) {
+	switch lex.token {
+	case scanner.Ident:
+		if lex.text() == "nil" {
+			v.Set(reflect.Zero(v.Type()))
+			lex.next()
+			return
+		}
+	case scanner.String:
+		s, _ := strconv.Unquote(lex.text())
+		v.SetString(s)
+		lex.next()
+		return
+	case scanner.Int:
+		i, _ := strconv.Atoi(lex.text())
+		v.SetInt(int64(i))
+		lex.next()
+		return
+	case '(':
+		lex.next()
+		readList(lex, v)
+		lex.next() // consume ')'
+		return
+	}
+	panic(fmt.Sprintf("unexpected token %q", lex.text()))
+}
+
+func readList(lex *lexer, v reflect.Value) {
+	switch v.Kind() {
+	case reflect.Array:
+		for i := 0; !endList(lex); i++ {
+			read(lex, v.Index(i))
+		}
+	case reflect.Slice:
+		for !endList(lex) {
+			item := reflect.New(v.Type().Elem()).Elem()
+			read(lex, item)
+			v.Set(reflect.Append(v, item))
+		}
+	case reflect.Struct: // ((name value) ...)
+		// field name -> value
+		fields := make(map[string]reflect.Value)
+		for i := 0; i < v.NumField(); i++ {
+			fieldInfo := v.Type().Field(i) // a reflect.StructField
+			tag := fieldInfo.Tag           // a reflect.StructTag
+			name := tag.Get("sexpr")
+			if name == "" {
+				name = strings.ToLower(fieldInfo.Name)
+			}
+			fields[name] = v.Field(i)
+		}
+
+		for !endList(lex) {
+			lex.consume('(')
+			if lex.token != scanner.Ident {
+				panic(fmt.Sprintf("got token %q, want field name", lex.text()))
+			}
+			name := lex.text()
+			lex.next()
+			read(lex, fields[name])
+			lex.consume(')')
+		}
+	case reflect.Map:
+		v.Set(reflect.MakeMap(v.Type()))
+		for !endList(lex) {
+			lex.consume('(')
+			key := reflect.New(v.Type().Key()).Elem()
+			read(lex, key)
+			value := reflect.New(v.Type().Elem()).Elem()
+			read(lex, value)
+			v.SetMapIndex(key, value)
+			lex.consume(')')
+		}
+	default:
+		panic(fmt.Sprintf("cannot decode list into %v", v.Type()))
+	}
+}
+
+func endList(lex *lexer) bool {
+	switch lex.token {
+	case scanner.EOF:
+		panic("end of file")
+	case ')':
+		return true
+	}
+	return false
+}
---
+++ ch12/ex13/sexpr/decode_test.go
@@ -0,0 +1,65 @@
+package sexpr
+
+import (
+	"bytes"
+	"reflect"
+	"testing"
+)
+
+func TestUnmarshal(t *testing.T) {
+	type Movie struct {
+		Title    string            `sexpr:"title"`
+		Subtitle string            `sexpr:"subtitle"`
+		Year     int               `sexpr:"year"`
+		Actor    map[string]string `sexpr:"actors"`
+		Oscars   []string          `sexpr:"oscars"`
+		Sequel   *string           `sexpr:"sequel"`
+	}
+	input := []byte(`
+	((title "Dr. Strangelove")
+	 (subtitle "How I Learned to Stop Worrying and Love the Bomb")
+	 (year 1964)
+	 (actors (("Brig. Gen. Jack D. Ripper" "Sterling Hayden")
+	          ("Dr. Strangelove" "Peter Sellers")
+	          ("Gen. Buck Turgidson" "George C. Scott")
+	          ("Grp. Capt. Lionel Mandrake" "Peter Sellers")
+	          ("Maj. T.J. \"King\" Kong" "Slim Pickens")
+	          ("Pres. Merkin Muffley" "Peter Sellers")))
+	 (oscars ("Best Actor (Nomin.)"
+	          "Best Adapted Screenplay (Nomin.)"
+	          "Best Director (Nomin.)"
+	          "Best Picture (Nomin.)"))
+	 (sequel nil))
+	`)
+
+	want := Movie{
+		Title:    "Dr. Strangelove",
+		Subtitle: "How I Learned to Stop Worrying and Love the Bomb",
+		Year:     1964,
+		Actor: map[string]string{
+			"Dr. Strangelove":            "Peter Sellers",
+			"Grp. Capt. Lionel Mandrake": "Peter Sellers",
+			"Pres. Merkin Muffley":       "Peter Sellers",
+			"Gen. Buck Turgidson":        "George C. Scott",
+			"Brig. Gen. Jack D. Ripper":  "Sterling Hayden",
+			`Maj. T.J. "King" Kong`:      "Slim Pickens",
+		},
+		Oscars: []string{
+			"Best Actor (Nomin.)",
+			"Best Adapted Screenplay (Nomin.)",
+			"Best Director (Nomin.)",
+			"Best Picture (Nomin.)",
+		},
+	}
+
+	var got Movie
+	//err := Unmarshal(input, &got)
+	decoder := NewDecoder(bytes.NewReader(input))
+	err := decoder.Decode(&got)
+	if err != nil {
+		t.Errorf("error: %v", err)
+	}
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got:\n%#v\nwant:\n%#v", got, want)
+	}
+}
